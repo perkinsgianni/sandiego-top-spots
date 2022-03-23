f04da588d7ae0f5f39d5535317e2156566c01592
giannip <perkinsgianni@gmail.com>
2022-03-23 11:50:25 -0400

    Add .DS_Store to .gitignore, modify index.html and main.js, add README.md, style.css, and project photos

---
+++ README.md
@@ -0,0 +1,4 @@
+![Project photo 1](assets/top_spots.png)
+![Project photo 2](assets/top_spots2.png)
+![Project photo 3](assets/top_spots3.png)
+![Project photo 4](assets/top_spots4.png)
--- main.js
+++ main.js
@@ -1,4 +1,22 @@
+// jQuery function to detect DOM's "readiness" to execute code
 $(document).ready(function() {
-// write your code here
+    // load data from data.json file using a GET JSON request, execute callback function if request succeeds
+    $.getJSON("data.json", function(data) {
+        // iterate through top spots 
+        data.reverse().forEach((topSpot) => {
+            // deconstruct each spot
+            let {
+                name,
+                description,
+                location: [lon, lat]
+            } = topSpot;
 
+            // create table row for each spot
+            $("#topSpots").after(
+                `<tr><td>${name}</td>
+                     <td>${description}</td>
+                     <td id="location-cell"><a href="https://www.google.com/maps?q=${lon},${lat}"><button id="location-button">Open in Google Maps</button></a></td></tr>`
+            )
+        });
+    });
 });
